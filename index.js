f9d7bad5f822a71f1940f8f61b56f989c07365e9
peter <pfranciskoe@gmail.com>
2020-08-20 10:59:13 -0700

    layout progress

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 console.log('hello world')
 document.addEventListener('DOMContentLoaded', () => {
     const navArrow = document.getElementById('nav-arrow')
+    const page1 = document.getElementById('page-1')
+    navArrow.addEventListener("click",()=>{scrollPage(page1.scrollHeight)})
 })
+function scrollPage(height){
+    window.scroll({
+        top: window.scrollY+height,
+        behavior:"smooth"
+    })
+}
